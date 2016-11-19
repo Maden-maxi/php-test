b241fb07b86198b64bb2e5c496e3d8854959a1e0
Maden-maxi <maden.maxi@yandex.ru>
2016-11-19 15:09:24 +0200

    Complite

--- src/form.php
+++ src/form.php
@@ -1,59 +1,130 @@
 <?php
+$fields = ['username', 'email', 'tel'];
+foreach ( $fields as $value){
+	if (!isset($_POST[$value])) {
+		echo "Bad request!";
+		die;
+	}
+}
 
 /**
+* Class for validating data
+* @author Denys Dnischenko <den0296@gmail.com>
 * @version 1.0.0
 */
 class Validator
 {	
 	/**
-	 * @var $fieldname str Name for detect were output $errorMsg
+	 * Name for detect were output $errorMsg
+	 * @var $fieldname string
 	 */
 	public $fieldname;
 	/**
-	 * @var $errorMsg str Error Message to output
+	 * Error Message to output
+	 * @var $errorMsg string
 	 */
 	public $errorMsg;
 	/**
-	 * @var $errorCode int Error code.For detect which message output
+	 * Error status.For detect which message output
+	 * @var $errorStatus string
 	 */
-	public $errorCode;
-	/**
-	 * @param $fieldname str
-	 * @param $value mixed(str,int) 
-	 */
-	function __construct($fieldname, ,$value)
+	public $errorStatus;
+
+    /**
+     * Validator constructor.
+     * @param $type string
+     * @param $value string
+     */
+	public function __construct($type ,$value)
 	{
-		
+	    $this->fieldname = $type;
+		switch ($type) {
+			case 'username':
+				self::name($value);
+				break;
+			case 'email':
+				self::email($value);
+				break;
+			case 'tel':
+				self::tel($value);
+				break;	
+			default:
+				echo "method not defined";
+				break;
+		}
 	}
-	function name($value)
+
+    /**
+     * Validate user name
+     * @param string $value
+     * @param int $min
+     * @param int $max
+     */
+	protected function name($value, $min = 6, $max = 12)
 	{
-		
+		if( is_string($value) ) {
+			//$pattern = preg_match("/^[\w]{6,12}$/", $value);
+			if( $value == '' ) {
+				$this->errorStatus = 'empty';
+				$this->errorMsg = 'Поле пустое,заполните пожалуйста поле';
+			}elseif ( strlen( $value ) < $min ) {
+				$this->errorStatus = 'few chars';
+				$this->errorMsg = 'Имя слишком короткое';
+			}elseif ( strlen( $value ) > $max ) {
+				$this->errorStatus = 'many chars';
+				$this->errorMsg = 'Имя слишком длинное';
+			}else{
+				$this->errorStatus = 'success';
+				$this->errorMsg = 'Поле заполнено правильно';
+			}
+		} else{
+			$this->errorStatus = 'type error';
+			$this->errorMsg = 'Введите буквение символи';
+		}
+	}
+
+    /**
+     * Validate email
+     * @param string $value
+     */
+	protected function email( $value )
+	{
+		if( is_string($value) ){
+			if( !preg_match("/^[a-zA-Z0-9_\-.]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-.]+$/", $value) ) {
+				$this->errorStatus = 'error';
+				$this->errorMsg = 'Неверно введений email. Пример как надо "my@mail.com".';
+			} else {
+				$this->errorStatus = 'success';
+				$this->errorMsg = 'Поле заполнено правильно';
+			}
+		}
+	}
+
+    /**
+     * Validate Telephone number
+     * @param string $value
+     */
+	protected function tel( $value )
+	{
+	    //$pattern = "/^380 \(([\d]{2})\) ([\d]{3})-([\d]{2})-([\d]{2})$/";
+        //#^380 \(([0-9]{2})\( ([0-9]{3})-([0-9]{2})-([0-9]{2})$#
+        if ( !preg_match("/^380 \(([\d]{2})\) ([\d]{3})-([\d]{2})-([\d]{2})$/", $value) ) {
+            $this->errorStatus = 'error';
+            $this->errorMsg = 'Неправильний формат.Пример "380 (22) 333-22-22" (Вместе с пробалами).';
+        } else {
+            $this->errorStatus = 'success';
+            $this->errorMsg = 'Поле заполнено верно';
+        }
+
 	}
 
 }
-$errorMsg = '';
-if ( isset($_POST['username']) ) {
-
-	if( strlen($_POST['username']) < 6 ){
-		$errorMsg = "Имя слишком короткое";
-	} elseif ( strlen($_POST['username']) > 12) {
-		$errorMsg = "Имя слишком длинное";
-	}
+// Pushing all fields in one array
+$fieldsValidateResult = [];
+foreach ($fields as $value) {
+    $fieldsValidateResult[] = new Validator( $value, $_POST[$value] );
 }
-echo $errorMsg;
-
-if ( isset( $_POST['email'] ) ) {
-
-	$pattern = "/@/";
-	preg_match("/^[a-zA-Z0-9_\-.]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-.]+$/", $_POST['email']);
-	if( preg_match($pattern, $_POST['email'] ) ){
-		echo "совпало email";
-	}
-
-}
-if ( isset($_POST['tel']) ) {
-	$pattern = "/^380 \(([\d]{2})\) ([\d]{3})-([\d]{2})-([\d]{2})$/";
-	if( preg_match($pattern, $_POST['tel']) ){
-		echo "Совпало tel";
-	}
-}
+// doing sleep for watch how spin spinner on the form
+sleep(3);
+// Encode our array object to JSON data for client side
+echo json_encode($fieldsValidateResult);
--- src/js/app.js
+++ src/js/app.js
@@ -57,19 +57,49 @@
 	$(document).on('submit', '#form', function(event) {
 		event.preventDefault();
 		/* Act on the event */
+		$('.spin-wrapper').removeClass('hidden');
 		var data = $(this).serialize();
 		$.ajax({
 			url: 'form.php',
 			type: 'POST',
 			data: data,
-			success: function (data, textStatus, xhr) {
-				console.log(data);
-				alert('success')
+			success: function (data) {
+				let response = $.parseJSON(data);
+				console.log(response);
+				var successes = 0;
+				$('.error-info').each(function (i) {
+					if(response[i].errorStatus != 'success'){
+						$(`#${response[i].fieldname}`).siblings('.error-info').text(response[i].errorMsg);
+					} else if(response[i].errorStatus == 'success') {
+						successes++;
+						$(`#${response[i].fieldname}`).siblings('.error-info').text("");
+					}
+					console.log($(`#${response[i].fieldname}`).next(),response[i].errorMsg);
+					console.log(successes,response.length);
+				});
+				if(response.length == successes){
+					console.log('reset');
+					//document.getElementById("form").reset();
+					$("#form input").val("");
+					$("#form .error-info").val("");
+					$("#success-notify").fadeIn("1000");
+					setTimeout(function () {
+						$("#success-notify").fadeOut("1000");
+						setTimeout(function () {
+							$("#modal-close").click();
+						}, 1000)
+					},3000);
+
+
+				}
+				$('.spin-wrapper').addClass('hidden');
+
 			},
 			error: function (xhr, textStatus, errorThrown) {
-				console.log(xhr);
+				console.log(xhr, textStatus, errorThrown);
 				alert('error');
 			}
 		});
+		return false;
 	});
 })(jQuery)
